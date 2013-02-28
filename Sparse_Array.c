84da971a6179b28d243e71ce98ee25ce159c9b0e
yew011 <yew011@ucsd.edu>
2013-02-28 01:41:25 -0800

    test starts

--- Sparse_Array.c
+++ Sparse_Array.c
@@ -23,13 +23,13 @@ struct Trie{
 };  
 
 // the constructor function.
-trie * construt_trie_(){
+trie * construct_trie_(){
   trie * rt_ = (trie *)malloc( sizeof(trie) );
   if( rt_ == NULL ){
     fprintf( stderr, "Error: malloc in constructor.\n" );
     exit( -1 );
-  } 
-  (rt_->content_).children_ == NULL;
+  }
+  (rt_->content_).children_ = NULL;
   return rt_;
 }
 
@@ -47,7 +47,7 @@ void destruct_trie_( trie * node_ ){
 }
 
 // the insert function.
-int insert( trie * root_, unsigned int idx_, int value_ ){
+int insert_( trie * root_, unsigned int idx_, int value_ ){
   if( root_ == NULL ) return -1;
   trie * tmp_ = root_;
   int i = 0;
@@ -67,9 +67,10 @@ int insert( trie * root_, unsigned int idx_, int value_ ){
 }
 
 // the get function.
-int get( trie * root_, unsigned int idx_ ){
+int get_( trie * root_, unsigned int idx_ ){
   trie * tmp_ = root_;
   if( tmp_ == NULL || 
+      (tmp_->content_).children_ == NULL ||
       (tmp_ = (tmp_->content_).children_[idx_&0xFF]) == NULL ||
       (tmp_ = (tmp_->content_).children_[(idx_>>8)&0xFF]) == NULL ||
       (tmp_ = (tmp_->content_).children_[(idx_>>16)&0xFF]) == NULL ||
@@ -90,11 +91,30 @@ int iterate_( trie * root_ ){
   if( root_ == NULL || (root_->content_).children_ == NULL )
     return -1;
   iterate_print_( root_, (unsigned int) 0 );
+  fprintf( stdout, "\n" );
   return 0;
 }
 
-// main function.
+// main function, for testing.
 int main( int argc, char ** argv ){
+  trie * test_trie_ = NULL;
+
+  /* test cases:
+     Tests repeated for empty trie_, trie_ only least 8 bits filled, trie_ sparsely filled.
+     1. Test the creation of trie_.
+     2. Test the insert of trie_.
+     3. Test the get of trie_.
+     4. Test the iterate of trie_.
+     5. Test the destructor of trie_.
+   */
+
+  // Test for empty trie_.
+  test_trie_ = construct_trie_();
+  assert( test_trie_ != NULL );
+  assert( (test_trie_->content_).children_ == NULL );
+  assert( get_(test_trie_, 1024) == 0 );
+  destruct_trie_(test_trie_);
+  fprintf( stdout, "Success: Test Create/Delete Empty Spare Array.\n" );
 
   return 0;
 }
